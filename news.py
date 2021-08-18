54e25131ad5666fdb50bd9380302f9e449aaf963
Juvens St Louis <juvensstlouis77@gmail.com>
2021-08-18 13:59:29 -0300

    feat: adding class News

---
+++ news.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.support.expected_conditions import title_contains
+
+
+class News:
+
+    def __init__(self, title, url):
+        self.title = title
+        self.url = url
+
+    def get_title(self):
+        return self.title
+
+    def get_url(self):
+        return self.url
