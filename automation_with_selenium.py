550e961a53e3bec55baa2a62453c3ba8d21fdd1a
Juvens St Louis <juvensstlouis77@gmail.com>
2021-08-18 00:49:19 -0300

    refac: adding wait and adjustments

--- automation_with_selenium.py
+++ automation_with_selenium.py
@@ -3,33 +3,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-
 
 driver = webdriver.Firefox()
-#driver.maximize_window()
+driver.maximize_window()
 
 driver.get('https://oblumenauense.com.br/')
 
-searchDropdown = driver.find_element(By.XPATH, '//*[@id="tdi_37"]/div/div/div/div[2]/div/div/div/div[2]/div/a')
-searchDropdown.click()
-    
-    
-searchText = driver.find_element(By.XPATH, '//*[@id="tdi_37"]/div/div/div/div[2]/div/div/div/div[2]/div/div/div/form/div/input')
-searchText.send_keys('saude', Keys.ENTER)
-    
-titulos_noticias = driver.find_elements(By.CSS_SELECTOR, '#tdi_76 .entry-title a')
+wait = WebDriverWait(driver, 10)
 
-for titulo in titulos_noticias: 
-    print('Tag Name!')
-    print(titulo.tag_name)
+wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.tdb-head-search-btn')))
+
+search_dropdown = driver.find_element(By.CSS_SELECTOR, '.tdb-head-search-btn')
+search_dropdown.click()    
+    
+wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.tdb-head-search-form-input')))    
+
+search_text = driver.find_element(By.CSS_SELECTOR, '.tdb-head-search-form-input')
+search_text.send_keys('saude', Keys.ENTER)
+    
+wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.tdb-title-text')))
+
+news_headlines = driver.find_elements(By.CSS_SELECTOR, '#tdi_76 .entry-title a')
+
+for headline in news_headlines: 
     print('TITLE')
-    print(titulo.get_attribute("title"))
+    print(headline.get_attribute("title"))
     print('HREF')
-    print(titulo.get_attribute("href"))
-    print('ELEMENTO')
-    print(titulo)
-
-
-
-
+    print(headline.get_attribute("href"))
