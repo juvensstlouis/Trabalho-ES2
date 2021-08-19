4d03217a91765698d3fa78f229b36bb7c3bf78bc
Juvens St Louis <juvensstlouis77@gmail.com>
2021-08-18 22:08:46 -0300

    feat: recording the news in an site

--- automation_with_selenium.py
+++ automation_with_selenium.py
@@ -5,35 +5,61 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from news import News
 
-driver = webdriver.Firefox()
-driver.maximize_window()
+try:
+    driver = webdriver.Firefox()
+    driver.maximize_window()
 
-driver.get('https://oblumenauense.com.br/')
+    driver.get('https://oblumenauense.com.br/')
 
-wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 10)
 
-search_dropdown = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.tdb-head-search-btn')))
-search_dropdown.click()    
-    
-search_text = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.tdb-head-search-form-input')))    
-search_text.send_keys('covid', Keys.ENTER)
-    
-#TODO: pegar a data de publicação também
-news_headlines = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '#tdi_76 .entry-title a')))
+    button_search_dropdown = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.tdb-head-search-btn')))
+    button_search_dropdown.click()    
+        
+    input_search_text = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.tdb-head-search-form-input')))    
+    input_search_text.send_keys('covid', Keys.ENTER)
+        
+    wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '.tdi_73 > div:nth-child(2)')))
 
-news_list = []
+    #TODO: pegar a data de publicação também
+    news_headlines = driver.find_elements(By.CSS_SELECTOR, '#tdi_76 .entry-title a')
 
-for headline in news_headlines: 
-    title = headline.get_attribute('title')
-    url = headline.get_attribute('href')
-    news_list.append(News(title, url))
+    news_list = []
 
-driver.get('http://dontpad.com/noticias/oblumenauense/')
+    for headline in news_headlines: 
+        title = headline.get_attribute('title')
+        url = headline.get_attribute('href')
+        news_list.append(News(title, url, '11/11/1111'))
 
-text_area = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#text')))
+    url_editor = 'iraklis-dokimi'
+    edit_code = 'iraklis-dokimi'
 
-text_area.clear()
-text_area.send_keys("Últimas 10 notícias sobre covid do site OBlumenauense (https://oblumenauense.com.br)\n\n")
+    driver.get('https://rentry.co/' + url_editor)
 
-for news in news_list: 
-    text_area.send_keys("Título: {} \nLink:{}\n\n".format(news.get_title(), news.get_url()))
+    button_edit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.float-left')))
+    button_edit.click() 
+
+    text_area = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.CodeMirror-code'))) 
+    text_lines = text_area.find_elements(By.CSS_SELECTOR, '.CodeMirror-line') 
+
+    for i in range(0, len(text_lines)) :
+        text_area.send_keys(Keys.SHIFT, Keys.ARROW_DOWN)
+
+    text_area.send_keys(Keys.BACKSPACE)
+
+    text_area.send_keys("Notícia | Data", Keys.ENTER)
+    text_area.send_keys("------ | ------", Keys.ENTER)
+
+    for news in news_list:
+        row = "[{0}]({1}) | {2}".format(news.title, news.url, news.data)
+        text_area.send_keys(row, Keys.ENTER)
+
+    input_edit_code = driver.find_element(By.CSS_SELECTOR, '#id_edit_code')  
+    input_edit_code.send_keys(edit_code)
+
+    button_save = driver.find_element(By.CSS_SELECTOR, '#submitButton')
+    button_save.click()
+
+except Exception as e:
+    print(e)
+    driver.close()
