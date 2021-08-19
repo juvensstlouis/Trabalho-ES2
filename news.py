7676ebeacf563ccbd2f6d258bdcc58f517695ab9
Juvens St Louis <juvensstlouis77@gmail.com>
2021-08-18 22:07:24 -0300

    feat: adding field data to class News

--- news.py
+++ news.py
@@ -3,12 +3,16 @@ from selenium.webdriver.support.expected_conditions import title_contains
 
 class News:
 
-    def __init__(self, title, url):
+    def __init__(self, title, url, data):
         self.title = title
         self.url = url
+        self.data = data
 
     def get_title(self):
         return self.title
 
     def get_url(self):
         return self.url
+
+    def get_data(self):
+        return self.data
