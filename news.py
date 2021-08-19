6ba2bdba9251f5d71ecf5b7b42004911f2e7063c
Juvens St Louis <juvensstlouis77@gmail.com>
2021-08-18 23:36:50 -0300

    refac: removing getters from class news

--- news.py
+++ news.py
@@ -3,16 +3,7 @@ from selenium.webdriver.support.expected_conditions import title_contains
 
 class News:
 
-    def __init__(self, title, url, data):
+    def __init__(self, title, url, date):
         self.title = title
         self.url = url
-        self.data = data
-
-    def get_title(self):
-        return self.title
-
-    def get_url(self):
-        return self.url
-
-    def get_data(self):
-        return self.data
+        self.date = date
